43b6f92546fb6094de299fdd105925a08c96b8c4
Walid Wazini <wazini25@gmail.com>
2021-11-23 10:10:55 +0800

    vue3 first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+        <friend-contact></friend-contact>
+        <friend-contact></friend-contact>
+        <friend-contact></friend-contact>
+    </ul>
+  </section>
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123456789",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "altair",
+          name: "Altair",
+          phone: "0123456789",
+          email: "altair@localhost.com",
+        },
+        {
+          id: "luna",
+          name: "Lunafreya",
+          phone: "0123456789",
+          email: "lunafreya@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails" >Show Details</button>
+    <ul v-if="detailsAreVisible" >
+      <li><strong>Phone: </strong> {{ friend.phone }}</li>
+      <li><strong>Email: </strong> {{ friend.phone }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123456789",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+      toggleDetails(){
+          this.detailsAreVisible = !this.detailsAreVisible
+      }
+  }
+};
+</script>
